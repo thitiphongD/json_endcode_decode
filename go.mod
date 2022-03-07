1491df1b8b4336b0b9288ec0d00470898777f164
thitiphongD <80618380+thitiphongD@users.noreply.github.com>
2022-03-07 11:15:37 +0700

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/thitiphongD/json_encode_decode
+
+go 1.17
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+)
---
+++ junk.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+)
+
+type Message struct {
+	Name string
+	Data interface{}
+}
+
+type Channel struct {
+	Id   string
+	Name string
+}
+
+func main() {
+	recRawMsg := []byte(`{"name":"channel add",` +
+		`"data":{"name":"Hardware Support"}}`)
+	var recMessage Message
+	err := json.Unmarshal(recRawMsg, &recMessage)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%#v\n", recMessage)
+
+	if recMessage.Name == "channel add" {
+		channel, err := addChannel(recMessage.Data)
+		var sendMessage Message
+		sendMessage.Name = "channel add"
+		sendMessage.Data = channel
+		sendRawMsg, err := json.Marshal(sendMessage)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(sendRawMsg))
+	}
+}
+
+func addChannel(data interface{}) (Channel, error) {
+	var channel Channel
+
+	err := mapstructure.Decode(data, &channel)
+	if err != nil {
+		return channel, err
+	}
+
+	channel.Id = "1"
+	fmt.Printf("%#v\n", channel)
+	return channel, nil
+}
